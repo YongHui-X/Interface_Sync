46fd07ac1c1088e0fc63b569d3bd4431479f53aa
Yong Hui <65816706+TanYongHui-X@users.noreply.github.com>
2025-09-07 21:00:07 +0800

    ok

--- src/itemTypes/Contact.java
+++ src/itemTypes/Contact.java
@@ -1,5 +1,32 @@
 package itemTypes;
 
-public class Contact {
-
+public class Contact implements iSyncable{
+	private String name, mobile, email;
+	//No-arg constructor. lets you create a “blank” object first, then fill in its fields later.
+	//don’t know the values at creation time
+	public Contact() {}
+	//create and initialise the object, know the values already.
+	public Contact(String name, String mobile, String email) {
+		this.name = name;
+		this.mobile = mobile;
+		this.email = email;
+	}
+	
+	//implements isyncable interface to say who is the class
+	public String getCode() {
+		return "C";
+	}
+	//returns data inside this class
+	public String encode() {
+		return String.format("%s|%s|%s", name, mobile, email);
+	}
+	
+	public void decode(String str) {
+		String[]parts = str.split("\\|");
+		
+		name = parts[0];
+		mobile = parts[1];
+		email = parts[2];
+		System.out.println("Contact decoded successfully");
+	}
 }
---
+++ src/main/SyncEngine.java
@@ -0,0 +1,47 @@
+package main;
+import java.util.ArrayList;
+
+import itemTypes.Contact;
+import itemTypes.Event;
+import itemTypes.Task;
+import itemTypes.iSyncable;
+import itemTypes.Note;
+public class SyncEngine {
+	private ArrayList<String> syncList = new ArrayList<String>();
+	
+	//Every iSyncable object must have getCode(), encode().
+    //Adds a string in the format: "CODE:encodedData".
+	public void add(iSyncable item) {
+		syncList.add(item.getCode() + ":" + item.encode() );
+		//item.getClass() asks Java: “Hey, what is the actual type of this object right now?”
+		System.out.println(item.getClass().getSimpleName() + " encoded successfully.");
+	}
+	
+	//test to see if each item can decode itself
+	public void testSync() {
+		
+		for (String str: syncList) {
+			String[] parts = str.split(":");
+			
+			if (parts[0].equals("C")) {
+				//decode to get back a contact object
+				Contact contact = new Contact();
+				contact.decode(parts[1]);
+				
+			} else if (parts[0].equals("T")) {
+				//decode to get back a task object
+				Task task = new Task();
+				task.decode(parts[1]);
+				
+			} else if (parts[0].equals("E")) {
+				Event event = new Event();
+				event.decode(parts[1]);
+			
+			} else if (parts[0].equals("N")) {
+				Note note = new Note();
+				note.decode(parts[1]);
+
+			}
+		}
+	}
+}
